aec0a63bba536106c00e8be19d5bbc7401e511b1
ortiqboy2005 <ortiqboy0804@gmail.com>
2023-09-27 18:09:21 +0500

    sda

--- src/Components/NOVOSTI/Novosti.jsx
+++ src/Components/NOVOSTI/Novosti.jsx
@@ -13,6 +13,8 @@ import 'swiper/css/navigation';
 import { Navigation, Pagination } from 'swiper/modules';
 function Novosti() {
     return (
+        <div className='Novosti'>
+
         <div className='container'>
             <div className="gl_header">
             <div >
@@ -24,16 +26,16 @@ function Novosti() {
                 <Swiper
                     slidesPerView={3}
                     spaceBetween={20}
-
+                    
                     pagination={{
                         clickable: true,
                     }}
-
+                    
                     navigation={true}
-
+                    
                     modules={[Pagination, Navigation]}
                     className="mySwiper"
-                >
+                    >
                     <SwiperSlide> <img className='gl_img_11' src={img} alt="DOCTOR.D" /><div className='gl_karta1'><h1>Mастер-класс в исполнении Галлямова Эдуарда Абдулхаевича</h1>
                         <h2>26.07.2021</h2>
                         <p>С 19 по 21 апреля в АО «РСНПМЦУ» (Республиканский Специализированный
@@ -98,6 +100,7 @@ function Novosti() {
             </div>
             </div>
 
+            </div>
         </div>
     )
 }
