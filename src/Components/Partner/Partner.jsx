923658b829189fa1e8d94cc1e6a628141a43a639
namides <namides05@gmail.com>
2023-09-27 16:42:39 +0500

    add

--- src/Components/Partner/Partner.jsx
+++ src/Components/Partner/Partner.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import "./Partner.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Part1 from '../../assets/img/Part1.png'
+import Parts1 from '../../assets/img/'
+import Part2 from '../../assets/img/Part2.png'
+import Part3 from '../../assets/img/Part3.png'
+import Part4 from '../../assets/img/Part4.png'
+import Part5 from '../../assets/img/Part5.png'
+import Part6 from '../../assets/img/Part6.png'
+import Part7 from '../../assets/img/Part7.png'
+import Part8 from '../../assets/img/Part8.png'
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/grid";
-import "swiper/css/pagination";
+// import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 // import './styles.css';
 
 // import required modules
-import { Grid, Pagination, Navigation } from "swiper/modules";
+import { Grid, Navigation } from "swiper/modules";
 
 function Partner() {
   return (
@@ -28,34 +37,34 @@ function Partner() {
           // pagination={{
           //   clickable: true,
           // }}
-          modules={[Grid, Pagination, Navigation]}
+          modules={[Grid,Navigation]}
           className="mySwiperbox"
-        > className="inswap"
-          <SwiperSlide className="inswap">Slide 1</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 2</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 3</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 4</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 5</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 6</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 7</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 8</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
-          <SwiperSlide className="inswap">Slide 9</SwiperSlide>
+        > 
+          <SwiperSlide><img src={Part1} alt="" /></SwiperSlide>
+          <SwiperSlide><img src={Part2} alt="" /></SwiperSlide>
+          <SwiperSlide><img src={Part3} alt="" /></SwiperSlide>
+          <SwiperSlide><img src={Part4} alt="" /></SwiperSlide>
+          <SwiperSlide><img src={Part5} alt="" /></SwiperSlide>
+          <SwiperSlide><img src={Part6} alt="" /></SwiperSlide>
+          <SwiperSlide><img src={Part7} alt="" /></SwiperSlide>
+          <SwiperSlide><img src={Part8} alt="" /></SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
         </Swiper> 
       </div>
     </div>
