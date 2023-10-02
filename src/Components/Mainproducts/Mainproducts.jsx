788df248496b6d660e34d0c3dba5c250d0520aa9
ziyo2007 <qodirjonovz6@gmail.com>
2023-10-02 13:24:46 +0300

    dfsdf

--- src/Components/Mainproducts/Mainproducts.jsx
+++ src/Components/Mainproducts/Mainproducts.jsx
@@ -4,43 +4,57 @@ import './Mainproducts.scss'
 function Mainproducts() {
   return (
     <div className='mainproducts'>
-        <h2>ПРОДУКЦИЯ</h2>
-        <p> Гланая<span>продукция</span></p>
-        <nav>
-            <p>Фильтр:</p>
-      <div class="btn-group nav_btnLanguage" role="group">
-                    <img src="" alt="" />
-                    <p className='nav_btn_languagename'></p>
-                      <ul class="dropdown-menu">
-                        <li><a  class="dropdown-item" href="#">Русский</a></li>
-                        <li><a  class="dropdown-item" href="#">Uzbek</a></li>
-                        <li><a  class="dropdown-item" href="#">English</a></li>
-                      </ul>
-                      <button type="button" class="btn dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false"></button>
-                </div>
-                <div class="btn-group nav_btnLanguage" role="group">
-                    <img src="" alt="" />
-                    <p className='nav_btn_languagename'></p>
-                      <ul class="dropdown-menu">
-                        <li><a  class="dropdown-item" href="#">Русский</a></li>
-                        <li><a  class="dropdown-item" href="#">Uzbek</a></li>
-                        <li><a  class="dropdown-item" href="#">English</a></li>
-                      </ul>
-                      <button type="button" class="btn dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false"></button>
-                </div>
-                <div class="btn-group nav_btnLanguage" role="group">
-                    <img src="" alt="" />
-                    <p className='nav_btn_languagename'></p>
-                      <ul class="dropdown-menu">
-                        <li><a  class="dropdown-item" href="#">Русский</a></li>
-                        <li><a  class="dropdown-item" href="#">Uzbek</a></li>
-                        <li><a  class="dropdown-item" href="#">English</a></li>
-                      </ul>
-                      <button type="button" class="btn dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false"></button>
-                </div>
+       <div className="container">
+       <h2 className='mproduct__tittle'>ПРОДУКЦИЯ</h2>
+        <p className='main__p'> Гланая<span>/продукция</span></p>
+        <nav className='mainpr__nav'>
+            <p className='filt__prod'>Фильтр:</p>
+            <ul className='mproduct__list'>
+              <li className='p-list__item'>
+              <button type="button" class="btn1 dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false"></button>
+              </li>
+              <li  className='p-list__item'>  
+              <button type="button" class="btn1 dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false"></button>
+              </li>
+              <li className='p-list__item'>
+              <button type="button" class="btn1 dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false"></button>
+              </li>
+            </ul>
         </nav>
+       </div>
     </div>
   )
 }
 
 export default Mainproducts
+
+      // <div class="btn-group nav_btnLanguage" role="group">
+      //               <img src="" alt="" />
+      //               <p className='nav_btn_languagename'></p>
+      //                 <ul class="dropdown-menu">
+      //                   <li><a  class="dropdown-item" href="#">Русский</a></li>
+      //                   <li><a  class="dropdown-item" href="#">Uzbek</a></li>
+      //                   <li><a  class="dropdown-item" href="#">English</a></li>
+      //                 </ul>
+      //                 <button type="button" class="btn dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false"></button>
+      //           </div>
+      //           <div class="btn-group nav_btnLanguage" role="group">
+      //               <img src="" alt="" />
+      //               <p className='nav_btn_languagename'></p>
+      //                 <ul class="dropdown-menu">
+      //                   <li><a  class="dropdown-item" href="#">Русский</a></li>
+      //                   <li><a  class="dropdown-item" href="#">Uzbek</a></li>
+      //                   <li><a  class="dropdown-item" href="#">English</a></li>
+      //                 </ul>
+      //                 <button type="button" class="btn dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false"></button>
+      //           </div>
+      //           <div class="btn-group nav_btnLanguage" role="group">
+      //               <img src="" alt="" />
+      //               <p className='nav_btn_languagename'></p>
+      //                 <ul class="dropdown-menu">
+      //                   <li><a  class="dropdown-item" href="#">Русский</a></li>
+      //                   <li><a  class="dropdown-item" href="#">Uzbek</a></li>
+      //                   <li><a  class="dropdown-item" href="#">English</a></li>
+      //                 </ul>
+      //                 <button type="button" class="btn dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false"></button>
+      //           </div>
