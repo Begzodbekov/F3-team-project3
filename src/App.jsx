e9756e46e81ee4b02ee224487d7c309899e2e97a
mrhakimov7 <hakimovabdurazzoq2008@gmail.com>
2023-10-05 15:26:17 +0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Products from './Components/Products/Products';
 import Home from './pages/Home/Home';
 import Mahsulot from './pages/Mahsulot/Mahsulot';
 import Tavar from './pages/Tavar/Tavar';
+import Services from './pages/Services/Services';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/mahsulot' element={<Mahsulot/>}/>
         <Route path='/tavar' element={<Tavar/>}/>
-
+        <Route path='services'element={<Services/>}/>
       </Routes>
     </div>
   );
